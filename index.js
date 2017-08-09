c523a527c59026bfce26bf35092ccbe21a17abfd
Victor Li <victorli5674@yahoo.com>
2017-08-09 11:35:45 -0700

    figured out what the basic routes should be

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/', function(req, res, next) {
+	return res.status(200).send('this would be the homepage');
+});
+
+app.get('/api', function(req, res, next) {
+	return res.status(200).send('search text here');
+});
+
+app.get('/latest', function(req, res, next) {
+	return res.status(200).send('latest');
+});
+
+app.all('*', function(req, res, next) {
+	return res.status(404).send({
+		error: 'There has been an error with your search. Please follow the instructions on http://localhost:3000'
+	});
+});
+
 var port = 3000;
 app.listen(port, function() {
 	console.log('Listening on port ' + port);
